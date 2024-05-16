60641b223619ac647c1558d88ee0b02819c49c35
Andrew Magill <AndyMagill@Gmail.com>
2024-05-15 22:27:54 -0400

    added pagination, part 2

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -17,8 +17,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
     
     if (term) {
       params.set('query', term);
+      params.set('page', '1');
     } else {
       params.delete('query');
+      params.delete('page');
     }
     replace(`${pathname}?${params.toString()}`);
   }, 300);
